714e0eaf50570f7b1a94bb405c248e61278a7f1e
Nilesh-NS-11 <136571158+Nilesh-NS-11@users.noreply.github.com>
2024-01-23 15:16:35 +0530

    Add files via upload

---
+++ Consecutive Numbers.sql
@@ -0,0 +1,57 @@
+/*
+
+Consecutive Numbers
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+*/
+
+/* Write your PL/SQL query statement below */
+SELECT DISTINCT num as ConsecutiveNums FROM
+(SELECT id, num,
+LAG(num) OVER (ORDER BY id) as prevNum,
+LEAD(num) OVER (ORDER BY id) as nextNum
+FROM logs) l
+WHERE l.num = l.prevNum
+AND l.prevNum = l.nextNum
